dded7e267929642b5080c1b3649c3a920ae90b00
denis salvino <denis.salvino23@gmail.com>
2021-04-12 01:02:07 -0500

    updated readme

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,9 +1,12 @@
+// Front-end Browser JavaScript to manipulate DOM
+// Global note-related variables
 let noteTitle;
 let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+// If we are in the /notes route, get references to our DOM to save user input and show & hide notes
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -22,48 +25,34 @@ const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
+// activeNote is used to keep track of the note in the textarea, empty note object
 let activeNote = {};
 
-const getNotes = () =>
-  fetch('/api/notes', {
-    method: 'GET',
+
+// Fetch (DELETE) an existing note in notes array with specified id
+const deleteNote = (id) => {
+  return fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
   });
+}
 
-const saveNote = (note) =>
-  fetch('/api/notes', {
+// Fetch (POST) new note to our db.json file using our api
+const saveNote = (note) => {
+  return fetch('/api/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
   });
+}
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-const renderActiveNote = () => {
-  hide(saveNoteBtn);
-
-  if (activeNote.id) {
-    noteTitle.setAttribute('readonly', true);
-    noteText.setAttribute('readonly', true);
-    noteTitle.value = activeNote.title;
-    noteText.value = activeNote.text;
-  } else {
-    noteTitle.value = '';
-    noteText.value = '';
-  }
-};
-
+// Called when user clicks on Save Button. Saves the active note to the DB
+// (db.json). When the POST to /api/notes completes, then calls our two 
+// render notes functions to render existing notes and the active note.
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
@@ -93,6 +82,26 @@ const handleNoteDelete = (e) => {
   });
 };
 
+// Display the active note or allow user to enter a new one
+const renderActiveNote = () => {
+  hide(saveNoteBtn);
+
+  if (activeNote.id === undefined) {
+    noteTitle.removeAttribute('readonly');
+    noteText.removeAttribute('readonly');
+    noteTitle.value = '';
+    noteText.value = '';
+  } else if (activeNote.id) {
+    noteTitle.setAttribute('readonly', true);
+    noteText.setAttribute('readonly', true);
+    noteTitle.value = activeNote.title;
+    noteText.value = activeNote.text;
+  } else {
+    noteTitle.value = '';
+    noteText.value = '';
+  }
+};
+
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
@@ -100,12 +109,13 @@ const handleNoteView = (e) => {
   renderActiveNote();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
+// Sets the activeNote to an empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
 };
 
+// Shows and hides the save button as needed
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
@@ -114,9 +124,11 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-// Render the list of note titles
+// Renders the list of note titles
 const renderNoteList = async (notes) => {
+
   let jsonNotes = await notes.json();
+
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
@@ -167,14 +179,27 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+// Fetch (GET) existing notes from our db.json file using our api
+const getNotes = () => {  
+  return fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+// Get existing notes from the db and render them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
+// Listen for events when we're in the /notes route
 if (window.location.pathname === '/notes') {
+  // Add event listeners
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
 
+// Initial function call to get and render notes
 getAndRenderNotes();
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# note_taker
+ 
+# Note Taker
+## About
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)<br>
+This is a website, created from the backend to keep track of notes, tasks, etc
+
+
+### Table of Contents
+**[About](#about)**<br>
+**[Installation Instructions](#installation-instructions)**<br>
+**[Usage Information](#usage-information)**<br>
+**[Test Instructions](#test-instructions)**<br>
+**[Contribution Guidelines](#contribution-guidelines)**<br>
+**[Licenses](#licenses)**<br>
+**[Contact Me](#contact-me)**<br>
+
+## Installation Instructions
+n/a
+## Usage Information
+If you needed to keep track of tasks, you would go to this deployment and use it
+## Test Instructions
+go through the website and follow the instructions
+## Contribution Guidelines
+I'm having promise errors and I'm not sure how i'm able to go through it
+## Licenses
+MIT
+## Contact Me
+denis.salvino23@gmail.com<br>
+<https://github.com/dsalvino>
